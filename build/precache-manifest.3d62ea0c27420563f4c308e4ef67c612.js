1d81cedc9283f50881f45de135266b7c8de64200
Alex Rayo <alexrayo.net@gmail.com>
2020-11-18 14:28:01 -0600

    uploaded build folder

---
+++ build/precache-manifest.3d62ea0c27420563f4c308e4ef67c612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ceee048adb0905edca135489b131fa3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3fbbee59c24d179f5bbf",
+    "url": "./static/css/main.708f5da9.chunk.css"
+  },
+  {
+    "revision": "012c1c9e889cb52cde89",
+    "url": "./static/js/2.a94f650f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.a94f650f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fbbee59c24d179f5bbf",
+    "url": "./static/js/main.24b0b3fb.chunk.js"
+  },
+  {
+    "revision": "4230fcabcd834e179f5f",
+    "url": "./static/js/runtime-main.b04d40c4.js"
+  }
+]);
